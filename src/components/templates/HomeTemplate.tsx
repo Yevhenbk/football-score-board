9feabd1665b7bdfdd7c5505f2f8a788c641d6819
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-08 00:45:10 +0100

    Configure boilerplate

---
+++ src/components/templates/HomeTemplate.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+const HomeTemplate: NextPage = () => {
+  return (
+    <main 
+      className='flex justify-center items-center w-[100%] h-[100vh]'
+    >
+      <h1>Hello World</h1>  
+    </main>
+  )
+}
+
+export default HomeTemplate;
