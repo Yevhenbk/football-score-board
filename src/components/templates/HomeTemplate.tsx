a0358d1626467994626e07d82dd8a9d24a82c84d
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-09 01:06:38 +0100

    Add startGame function

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -3,13 +3,27 @@ import { Context } from '@store/context'
 
 const HomeTemplate: React.FC = () => {
 
-  const { testingVariable } = useContext(Context)
+  const { matches, startGame } = useContext(Context)
 
   return (
     <main 
-      className='flex justify-center items-center w-[100%] h-[100vh]'
+      className='flex flex-col justify-center 
+      items-center w-[100%] h-[100vh] gap-12'
     >
-      <h1>{testingVariable}</h1>  
+      <button 
+        onClick={() => startGame('Mexico', 'Canada')} 
+        data-testid='start-mexico-canada'
+      >
+        Start Mexico vs Canada
+      </button> 
+
+      <div>
+        <h2 className='text-xl text-center'>Matches</h2>
+        {matches.map((match: any, index: any) => (
+          <div key={index} data-testid={match}>
+            {match.homeTeam} {match.homeScore} - {match.awayTeam} {match.awayScore}
+          </div>))}
+      </div>
     </main>
   )
 }
--- src/store/context.tsx
+++ src/store/context.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 export const Context = createContext<any>('')
 
@@ -6,12 +6,34 @@ type Props = {
   children: React.ReactNode
 }
 
+type Match = {
+  homeTeam: string
+  awayTeam: string
+  homeScore: number
+  awayScore: number
+}
+
 const ContextProvider: React.FC<Props> = (props) => {
+
+  const [matches, setMatches] = useState<Match[]>([])
+
+  const startGame = (homeTeam: string, awayTeam: string) => {
+    const newMatch = {
+      homeTeam: homeTeam,
+      awayTeam: awayTeam,
+      homeScore: 0,
+      awayScore: 0
+    }
+    
+    setMatches(prevMatches => [...prevMatches, newMatch])
+  }
 
   const testingVariable: string = 'Hello World'
 
   return (
-    <Context.Provider value={{ testingVariable }}>
+    <Context.Provider 
+      value={{ testingVariable, matches, setMatches, startGame }}
+    >
       {props.children}
     </Context.Provider>
   )  
