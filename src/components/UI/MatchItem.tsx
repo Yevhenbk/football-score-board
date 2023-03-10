b0193e81f195d02c693b668fe5cd62d4eb42bb0f
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-10 14:37:48 +0100

    Style MatchItem

--- src/components/UI/MatchItem.tsx
+++ src/components/UI/MatchItem.tsx
@@ -11,12 +11,25 @@ interface Props {
 const MatchItem: FC<Props> = (props) => {
   return (
     <div
-      className='flex' 
+      className='flex flex-row w-[35rem] justify-between
+      text-2xl bg-[#4444] items-center py-4 px-8 rounded-[3px]' 
       data-testid={props.testId}
     >
-      <p>
-        {props.homeTeam} {props.homeScore} - {props.awayTeam} {props.awayScore}
-      </p>
+      <h6>
+        {props.homeTeam}
+      </h6>
+      <h6>
+        {props.homeScore}
+      </h6>
+      <h6>
+        -
+      </h6>
+      <h6>
+        {props.awayTeam}
+      </h6>
+      <h6>
+        {props.awayScore}
+      </h6>
     </div>
   )
 }
