edc5d2387cc8ef60f8e49d03285b78ecbab660d0
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-10 18:49:20 +0100

    MatchItem data update

--- src/components/UI/MatchItem.tsx
+++ src/components/UI/MatchItem.tsx
@@ -1,35 +1,47 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 
 interface Props {
   homeTeam: string,
   awayTeam: string,
   homeScore: number,
   awayScore: number,
-  testId: string
+  testId: string,
+  children: ReactNode
 }
 
 const MatchItem: FC<Props> = (props) => {
   return (
     <div
-      className='flex flex-row w-[35rem] justify-between
+      className='flex flex-col w-[35rem] justify-center
       text-2xl bg-[#4444] items-center py-4 px-8 rounded-[3px]' 
       data-testid={props.testId}
     >
-      <h6>
-        {props.homeTeam}
-      </h6>
-      <h6>
-        {props.homeScore}
-      </h6>
-      <h6>
-        -
-      </h6>
-      <h6>
-        {props.awayTeam}
-      </h6>
-      <h6>
-        {props.awayScore}
-      </h6>
+      <div
+        className='flex flex-row justify-between
+        items-center w-[100%]'
+      >
+        <h6>
+          {props.homeTeam}
+        </h6>
+        <h6>
+          {props.homeScore}
+        </h6>
+        <h6>
+          -
+        </h6>
+        <h6>
+          {props.awayTeam}
+        </h6>
+        <h6>
+          {props.awayScore}
+        </h6>
+      </div>
+      <div
+        className='flex flex-row justify-between
+        items-center w-[100%]'
+      >
+        {props.children}
+      </div>
     </div>
   )
 }
