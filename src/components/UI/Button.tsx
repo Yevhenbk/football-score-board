392a8e69a8e0b756797db493431cf1430733d49b
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-10 19:45:15 +0100

    Fix in style

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -12,7 +12,7 @@ const Button: FC<Props> = (props) => {
       className='btn min-w-[10rem] h-12 bg-transparent
       text-white text-lg flex flex-row px-4
       justify-center items-center gap-4 m-0
-      hover:bg-pink-600 cursor-pointer'
+      hover:bg-pink-600 cursor-pointer text-center'
       onClick={props.onClick}
       data-testid={props.testId}
     >
