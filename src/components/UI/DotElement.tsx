b58b6fa2540ca09587a17fbee946cc8cb9c24912
Yevhenbk <balagutrak.jenia@gmail.com>
2023-03-10 01:29:00 +0100

    Create DotElement

---
+++ src/components/UI/DotElement.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const DotElement: FC = () => {
+  return (
+    <div 
+      className='w-[.35rem] h-[.35rem] bg-white rounded-full'
+    />
+  )
+}
+
+export default DotElement
